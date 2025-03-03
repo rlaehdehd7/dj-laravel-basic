864bd8e61d03d5c7dcc335b49efc55c0b8bafa6e
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-03-03 14:28:01 +0900

    12강까지

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,7 +7,7 @@
             <h1 class="text-2xl">글쓰기</h1>
             <form action="/articles" method="POST" class="mt-3">
                 @csrf
-                <input type="text" name='body' class="block w-full mb-2 rounded">
+                <input type="text" name='body' class="block w-full mb-2 rounded" value="{{ old('body') }}">
                 @error('body')
                     <p class="text-xs text-red-500 mb-3"> {{ $message }} </p>
                 @enderror
